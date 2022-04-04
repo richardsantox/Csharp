ae26f2cb4f34c5d80743741bb10579b73372e273
richardsantox <richardsantospereira4322@gmail.com>
2022-04-04 11:56:43 -0300

    Exemplos de introdução

---
+++ VS/Aula3VS/Aula3VS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula3VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome: ");
+            string nome  = Console.ReadLine();
+
+            Console.WriteLine("Entre com sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nOi " + nome + "\nQue legal que vc tem " + idade + " anos de idade");
+
+        }
+    }
+}
---
+++ VScode/Aula3VSC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula3VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+            float peso;
+            float imc;
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digte sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu peso: ");
+            peso = (float) Convert.ToInt32(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + imc);
+        }
+    }
+}
