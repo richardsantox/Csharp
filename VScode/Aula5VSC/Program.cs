43585a4071a12c6c1a9e45985867c3e3998ea944
richardsantox <richardsantospereira4322@gmail.com>
2022-04-05 12:51:00 -0300

    @richardsantox {Aula: 5}

---
+++ VScode/Aula5VSC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula5VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com valor vendido mensal: ");
+            Double valorVendido = double.Parse(Console.ReadLine());
+
+            if(valorVendido <= 10000)
+            {
+                Console.WriteLine("Valor a receber: R$" + (valorVendido * 0.01));
+            }
+            else if(valorVendido <= 20000)
+            {
+                Console.WriteLine("O valor a receber: R$ " + (valorVendido * 0.02));
+            }
+             else if(valorVendido <= 30000)
+            {
+                Console.WriteLine("O valor a receber: R$ " + (valorVendido * 0.03));
+            }
+             else 
+            {
+                Console.WriteLine("O valor a receber: R$ " + (valorVendido * 0.1));
+            }
+        }
+    }
+}
