6bae8ee851782d3fe111d5a351ccb6c211a4f0f9
richardsantox <richardsantospereira4322@gmail.com>
2022-04-05 15:17:48 -0300

    Atividade Aula

---
+++ VS/Aula5VS/Aula5VS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exemplo1BoazAula2Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Você concluiu o curso(sim ou não): ");
+            string condicaoCursoConcluido = Console.ReadLine();
+
+            Console.WriteLine("Quantos cursos você concluiu?: ");
+            int condicaoQuantidadeCursoConcluido = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você quitou o curso?(sim ou não): ");
+            string condicaoQuitacao = Console.ReadLine();
+
+            Console.WriteLine("Você todos os livros?(sim ou não): ");
+            string condicaobiblioteca = Console.ReadLine();
+
+
+            if (condicaoCursoConcluido == "sim" && condicaoQuantidadeCursoConcluido >= 25 &&
+                condicaoQuitacao == "sim" && condicaobiblioteca == "sim")
+            {
+                Console.WriteLine("Candidato aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Candidato reprovado");
+            }
+        }
+    }
+}
---
+++ VS/Calculo_de_IMC/Calculo_de_IMC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculo_de_IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual a sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual o seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual é a sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            if (imc < 18.5) 
+            {
+                Console.WriteLine("Sua situação " + nome + ": Abaixo do peso"); 
+            }
+            else if (imc < 25)
+            {
+                Console.WriteLine("Sua situação " + nome + ": peso normal");
+            }
+            else if (imc < 30)
+            {
+                Console.WriteLine("Sua situação " + nome + ": Sobrepeso");
+            }
+            else 
+            {
+                Console.WriteLine("Sua situação " + nome + ": Obesidade");
+            }
+        }
+    }
+}
