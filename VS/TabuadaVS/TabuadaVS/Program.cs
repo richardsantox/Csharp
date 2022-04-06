ba5ccf0fc9645751cc39f66a4504f23b6c2f1a8d
richardsantox <richardsantospereira4322@gmail.com>
2022-04-06 15:32:01 -0300

    @richardsantox {Atividade Tabuada}

---
+++ VS/TabuadaVS/TabuadaVS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TabuadaVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um numero para tabuada: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada do: {num}");
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.WriteLine(num + "x" + y + " = " + num * y);
+            }
+            
+        }
+    }
+}
