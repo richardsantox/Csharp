b624c77a3e99247f97b16b2ef3e38530f1ae3f3f
richardsantox <richardsantospereira4322@gmail.com>
2022-04-08 15:38:11 -0300

    @richardsantox {Aula 8} [Criando objetos]

---
+++ VS/Aula8VS/Aula8VS/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula8VS.src;
+using System;
+
+namespace Aula8VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Veiculos bmw = new Veiculos("X6", "Preta", "Carro", 4);
+            Veiculos honda = new Veiculos("CG Start", "Azul", "Moto", 2);
+            Veiculos rms = new Veiculos("Titanic", "Branco e Preto", "Navio", 0);
+            Veiculos boeing = new Veiculos("XB-15", "Cinza", "Avião", 3);
+
+            Console.WriteLine($"O veículo é o/a {bmw.Nome} de cor {bmw.Cor}, de tipo '{bmw.Tipo}'  e tem {bmw.QuantidadeRodas} rodas.");
+            Console.WriteLine($"O veículo é o/a {honda.Nome} de cor {honda.Cor}, de tipo '{honda.Tipo}' e tem {honda.QuantidadeRodas} rodas.");
+            Console.WriteLine($"O veículo é o/a {rms.Nome} de cor {rms.Cor}, de tipo '{rms.Tipo}' e tem {rms.QuantidadeRodas} rodas.");
+            Console.WriteLine($"O veículo é o/a {boeing.Nome} de cor {boeing.Cor}, de tipo '{boeing.Tipo}' e tem {boeing.QuantidadeRodas} rodas.\n");
+
+            Veiculos met = new Veiculos();
+            met.Ligar();
+            met.Andar();
+            met.Freiar();
+
+        }
+    }
+}
---
+++ VS/Aula8VS/Aula8VS/src/Veiculos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Aula8VS.src
+{
+    public class Veiculos
+    {
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public string Tipo { get; set; }
+        public int QuantidadeRodas { get; set; }
+
+        public Veiculos()
+        {
+
+        }
+
+        public Veiculos(string _nome, string _cor, string _tipo, int _qtdsRodas)
+        {
+            Nome = _nome;
+            Cor = _cor;
+            Tipo = _tipo;
+            QuantidadeRodas = _qtdsRodas;
+        }
+
+
+        public void Andar()
+        {
+            Console.WriteLine("Veículo andando: Vrummmm");
+        }
+        
+        public void Freiar()
+        {
+            Console.WriteLine("Veículo parando: Eeeepa");
+        }
+        public void Ligar()
+        {
+            Console.WriteLine("Veiculo ligando");
+        }
+    }
+}
