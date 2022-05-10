6681985c4a58c2fccc6e4fcabfda78b0d2e9c55f
richardsantox <richardsantospereira4322@gmail.com>
2022-05-10 08:18:45 -0300

    @richardsantox [Update em Limpeza de codigo] {10/05/2022}

---
+++ VScode/Aula11VSC/Lista/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listaa");
+
+            List<string> lista = new List<string>();
+
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Entre com o nome: ");
+                var nome = Console.ReadLine();
+                lista.Add(nome);
+            }
+
+            Console.WriteLine("Lista");
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Elemento: {lista[1]}");
+
+            
+            /*lista.Add("Yasmin");
+            lista.Add("Karol");
+            lista.Add("Correria");
+            lista.Add("Kaique");
+
+            foreach(string item in lista)
+            {
+                Console.WriteLine(item);
+            }*/
+
+        }
+    }
+}
